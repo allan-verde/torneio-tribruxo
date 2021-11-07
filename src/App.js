5410beafca116bb36d4ceb28c570de22d1c46211
Allan Verde <allangreen2906@gmail.com>
2021-11-07 15:12:51 -0300

    Concluindo renderizaçao

--- src/App.js
+++ src/App.js
@@ -1,21 +1,94 @@
-import './App.css'
-import Students from './components/Students'
-import { useState, useEffect } from 'react'
+import "./App.css";
+import Students from "./components/Students";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [api, setApi] = useState([])
+  const [api, setApi] = useState([]);
+  const [apiFiltred, setApiFiltred] = useState(undefined);
+  const [stud1, setStud1] = useState(undefined);
+  const [stud2, setStud2] = useState(undefined);
+  const [stud3, setStud3] = useState(undefined);
 
   useEffect(() => {
-    fetch('https://hp-api.herokuapp.com/api/characters/students')
-    .then((response) => response.json())
-    .then((response) => setApi(response))
-  }, [])
-      
+    fetch("https://hp-api.herokuapp.com/api/characters/students")
+      .then((response) => response.json())
+      .then((response) => setApi(response));
+  }, []);
+
+  useEffect(() => {
+    setApiFiltred(api);
+  }, [api]);
+
+  useEffect(() => {
+    if (typeof stud1 !== "object") {
+      setStud1(api[Math.floor(Math.random() * api.length)]);
+    } else if (apiFiltred.length === api.length) {
+      setApiFiltred(apiFiltred.filter((item) => item.house !== stud1.house));
+      return () => {};
+    }
+  }, [stud1, apiFiltred, api]);
+
+  useEffect(() => {
+    if (
+      typeof stud1 === "object" &&
+      apiFiltred.length < api.length &&
+      typeof stud2 !== "object"
+    ) {
+      setStud2(apiFiltred[Math.floor(Math.random() * apiFiltred.length)]);
+    }
+    if (
+      typeof stud2 === "object" &&
+      apiFiltred.filter((e) => e.name === stud2.name).length === 1
+    ) {
+      setApiFiltred(apiFiltred.filter((item) => item.house !== stud2.house));
+      return () => {};
+    }
+  }, [stud1, stud2, apiFiltred, api.length]);
+
+  useEffect(() => {
+    if (
+      typeof stud2 === "object" &&
+      apiFiltred.filter((e) => e.name === stud2.name).length === 0 &&
+      typeof stud3 !== "object"
+    ) {
+      setStud3(apiFiltred[Math.floor(Math.random() * apiFiltred.length)]);
+    }
+    if (
+      typeof stud3 === "object" &&
+      apiFiltred.filter((e) => e.name === stud3.name).length === 1
+    ) {
+      setApiFiltred(apiFiltred.filter((item) => item.house !== stud3.house));
+      return () => {};
+    }
+  }, [stud2, stud3, apiFiltred]);
+
   return (
     <div className="App">
-        <Students api={api}/>
+      <div className="App-menu">
+        {typeof stud3 === "object" ? (
+          <>
+            <Students stud={stud1} />
+            <Students stud={stud2} />
+            <Students stud={stud3} />
+          </>
+        ) : (
+          () => {}
+        )}
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setApiFiltred(api);
+            setStud1(undefined);
+            setStud2(undefined);
+            setStud3(undefined);
+          }}
+        >
+          Tente Novamente
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/Students/index.jsx
+++ src/components/Students/index.jsx
@@ -1,57 +1,30 @@
 import "./style.css";
-import { useEffect, useState } from "react";
 
-const Students = ({ api }) => {
-  const [apiFiltred, setApiFiltred] = useState(undefined);
-  const [stud1, setStud1] = useState(undefined);
-  const [stud2, setStud2] = useState(undefined);
-  const [stud3, setStud3] = useState(undefined);
-
-  useEffect(() => {
-    // NÃO MEXER
-    setApiFiltred(api);
-  }, [api]);
-
-  useEffect(() => {
-    if (stud1 !== undefined && stud2 === undefined) {
-      console.log("api-filter-1");
-      setApiFiltred(apiFiltred.filter((item) => item.house !== stud1.house));
-    } else if (stud2 !== undefined && stud3 === undefined) {
-      console.log("api-filter-2");
-      setApiFiltred(apiFiltred.filter((item) => item.house !== stud2.house));
-    } else if (stud3 !== undefined) {
-      console.log("api-filter-3");
-      setApiFiltred(apiFiltred.filter((item) => item.house !== stud3.house));
-    } else {
-      console.log("terminou");
-    }
-  }, [stud1, stud2, stud3]);
-
-  useEffect(() => {
-    setStud1(api[Math.floor(Math.random() * api.length)]);
-  }, [api]);
-
-  useEffect(() => {
-    if (stud1 !== undefined) {
-      setStud2(api[Math.floor(Math.random() * api.length)]);
-    }
-  }, [stud1, api]);
-
-  useEffect(() => {
-    if (stud2 !== undefined) {
-      setStud3(api[Math.floor(Math.random() * api.length)]);
-    }
-  }, [stud2, api]);
-
-  console.log(apiFiltred);
-  console.log("api-1", stud1);
-  console.log("api-2", stud2);
-  console.log("api-3", stud3);
-  console.log("-------------");
+const Students = ({ stud }) => {
   return (
-    <div>
-      <div>oi</div>
-      <button>add</button>
+    <div className="conteudo">
+      <div className="conteudo-img">
+        {stud.image !== "" ? (
+          <img src={stud.image} alt={stud.name} />
+        ) : (
+          <img
+            src="https://triunfo.pe.gov.br/pm_tr430/wp-content/uploads/2018/03/sem-foto.jpg"
+            alt="sem-avatar"
+          />
+        )}
+      </div>
+      <div className="conteudo-descricao">
+        <h3>
+          NAME: <span>{stud.name}</span>
+        </h3>
+        {stud.house !== "" ? (
+          <h3>
+            HOUSE: <span>{stud.house}</span>
+          </h3>
+        ) : (
+          <h3>HOUSE: Casa não definida</h3>
+        )}
+      </div>
     </div>
   );
 };
